a3a49fd4d77bf8422a7134123fc75d6ce7eab30d
lucascscape <lucasbr.santoscosta@outlook.com>
2020-10-20 00:01:18 -0300

    listar pronto

--- filmessenai/src/pages/Lista-filmes/index.tsx
+++ filmessenai/src/pages/Lista-filmes/index.tsx
@@ -1,25 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Footer from '../../components/footer';
-import Header from '../../components/header';
-import Mascaras from '../../assets/images/theater.png';
-import lixo from '../../assets/images/trash.png';
-import atual from '../../assets/images/refresh.png';
-import Input from '../../components/input/index';
-import Button from '../../components/button/index';
+import Header from '../../components/header-comun';
+import Mascaras from '../../assets/images/cinema.png';
 import '../../assets/styles/global.css';
 import './style.css';
 
 function Filmes() {
-    //prop string genero {get; set;} = "";
-    const [idfilme, setIdFilme] = useState(0);
-    const [filme, setFilme] = useState('');
-   
+
     const [filmes, setFilmes] = useState([]);
-    
-    //useEffect te permite executar efeitos colaterais em componentes funcionais
-    //Buscar dados é um exemplo de efeito colateral
-    //usando o useEffect informo ao React que o componente somente depois da renderização
-    //é executado depois da renderização do componente, quando ele já estiver montado na DOM
+   
     useEffect(() => {
       listar();
     }, []);
@@ -28,40 +17,14 @@ function Filmes() {
       fetch('http://localhost:5000/api/filmes', {
         method: 'GET',
         headers: {
-          //Bearer authentication é o token authentication, um Schema para autenticação HTTP 
-          //O Bearer identifica recursos protegidos por um OAuth2.
+      
           authorization: 'Bearer ' + localStorage.getItem('token-filmes')
         }
       })
         .then(response => response.json())
         .then(dados => {
           setFilmes(dados);
-        })
-        .catch(err => console.error(err));
-    }
-   
-   
-    const salvar = () => {
-      const form = {
-        nome: filme
-      };
-   
-      const method = (idfilme === 0 ? 'POST' : 'PUT');
-      const urlRequest = (idfilme === 0 ? 'http://localhost:5000/api/filmes' : 'http://localhost:5000/api/filmes/' + idfilme);
-   
-      fetch(urlRequest, {
-        method: method,
-        body: JSON.stringify(form),
-        headers: {
-          'content-type': 'application/json',
-          authorization: 'Bearer ' + localStorage.getItem('token-filmes')
-        }
-      })
-        .then(() => {
-          alert('Genero cadastrado');
-          setIdFilme(0);
-          setFilme('');
-          listar();
+         
         })
         .catch(err => console.error(err));
     }
@@ -71,39 +34,35 @@ function Filmes() {
         <Header description="Lista de filmes da nossa coletanea" />
    
       <main>
-        <h1>Lista de Filmes</h1>
-        <div className="imgTitulo">
-        <img className="theater" src={Mascaras} alt=""/>
-        </div>
-        <table>
+        <h1 className='h1'>Lista de Filmes</h1>
+        <img className='img'  src={Mascaras} alt=""/>
+        <table className='table'>
           <thead>
             <tr>
-              <th>Id</th>
-              <th>titulo</th>
+              <th>Titulo dos Filmes</th>
+              <th>Genero</th>
             </tr>
           </thead>
           <tbody>
+           
             {
               filmes.map((item: any) => {
                 return (
                   <tr key={item.idfilme}>
-                    <td>{item.idFilme}</td>
                     <td>{item.titulo}</td>
+                    <td>{item.genero.nome}</td>
                    
                   </tr>
                 )
               })
             }
+            
           </tbody>
         </table>
    
-        <form onSubmit={event => {
-          event.preventDefault();
-          salvar();
-        }}>
-         
-        </form>
         </main>
+        <br>
+        </br>
         <Footer/>
         
       </div>
